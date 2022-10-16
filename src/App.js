23a6b90535436a8e015c3bb2d4f88e1b5ed7febf
Joey Pettit <pettitbass@gmail.com>
2022-10-15 21:41:17 -0500

    getting dates to show up on calendar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+  const todaysDate = new Date();  // get today's date
+  const startingDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), 1) // get first of this month
+  console.log('first of the month', startingDate);
+
+  // create rest week before 1st (to Moday before), startingDate of October 2022 is Monday Sept 26
+  startingDate.setDate(startingDate.getDate() - (startingDate.getDay() - 1))
+  console.log('Monday before 1st of month', startingDate);
+
+  // create array of date objects for each day of this months view
+  const dates = []
+  for (let i = 0; i < (6 * 7); i++) {
+      const date = new Date(startingDate)
+      dates.push({ date, events: 'add events here' })
+      startingDate.setDate(startingDate.getDate() + 1)
+  }
+  console.log('dates array', dates);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Calendar Spike</h1>
+        <div id="cal-holder">
+          {dates.map((date, index)=>{
+            return(
+              <div 
+                key={index} 
+                className="cal-cell"
+              >
+                <div className="date">
+                  {date.date.getDate()}
+                </div>
+                <div>
+                  <p>{date.events}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
       </header>
     </div>
   );
